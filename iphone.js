26650da4e0175bb4a69f95b9a05b5980f2f12349
Danikkjsl <142660535+Danikkjsl@users.noreply.github.com>
2025-02-22 11:33:34 +0200

    Update iphone.js

--- iphone.js
+++ iphone.js
@@ -9,3 +9,15 @@ new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
 });
+
+const asideBlock16promax = document.querySelector('.aside-block')
+const asideBlock16promaxButton = document.getElementById('aside-block-16promax')
+
+asideBlock16promaxButton.addEventListener('click', ()=>{
+  asideBlock16promax.style.display = 'block';
+  document.body.style.filter = 'blur(5px)';
+  asideBlock16promax.style.filter = 'none';
+})
+function makeBlockVisible(){
+  asideBlock16promax.style.display = 'block'
+}
