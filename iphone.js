eed5635f5e7cd33fb006d14ae8c846817508adef
Danikkjsl <142660535+Danikkjsl@users.noreply.github.com>
2025-02-27 13:33:37 +0200

    Update iphone.js

--- iphone.js
+++ iphone.js
@@ -10,14 +10,41 @@ new Swiper('.swiper', {
     },
 });
 
-const asideBlock16promax = document.querySelector('.aside-block')
-const asideBlock16promaxButton = document.getElementById('aside-block-16promax')
+const asideBlock16promax = document.getElementById('aside-block1');
+const asideBlock16pro = document.getElementById('aside-block2');
+const asideBlock16 = document.getElementById('aside-block3');
+const asideBlock16promaxButton = document.getElementById('aside-block-16promax');
+const asideBlock16proButton = document.getElementById('aside-block-16pro');
+const asideBlock16Button = document.getElementById('aside-block-16');
+const overlay = document.createElement('div'); 
 
-asideBlock16promaxButton.addEventListener('click', ()=>{
-  asideBlock16promax.style.display = 'block';
-  document.body.style.filter = 'blur(5px)';
-  asideBlock16promax.style.filter = 'none';
+overlay.classList.add('overlay-blur'); 
+
+document.body.appendChild(overlay); 
+
+asideBlock16promaxButton.addEventListener('click', () => {
+    asideBlock16promax.style.display = 'block';
+    overlay.style.display = 'block'; 
+});
+asideBlock16proButton.addEventListener('click', () =>{
+  asideBlock16pro.style.display = 'block';
+  overlay.style.display = 'block'; 
 })
-function makeBlockVisible(){
-  asideBlock16promax.style.display = 'block'
+asideBlock16Button.addEventListener('click', () =>{
+  asideBlock16.style.display = 'block';
+  overlay.style.display = 'block'; 
+})
+
+function makeBlockVisible() {
+    asideBlock16promax.style.display = 'block';
+    overlay.style.display = 'block';
 }
+
+window.addEventListener('click',(e)=>{
+  if (e.target.classList.contains('overlay-blur')){
+    asideBlock16promax.style.display = 'none'
+    asideBlock16pro.style.display = 'none'
+    asideBlock16.style.display = 'none'
+  overlay.style.display = 'none'
+  }
+})
